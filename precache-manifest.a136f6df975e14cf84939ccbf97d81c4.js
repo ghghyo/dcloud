7bbff72c1edd8b16e7511756650e14056920b7c6
ghghyo <youssef.abubaker@outlook.com>
2022-02-21 12:11:28 -0500

    Updates

---
+++ precache-manifest.a136f6df975e14cf84939ccbf97d81c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed19b3652f3d3b356e0d951386700a3b",
+    "url": "/dcloud/index.html"
+  },
+  {
+    "revision": "ae4eb96ca59dba2679c6",
+    "url": "/dcloud/static/css/main.24f44fde.chunk.css"
+  },
+  {
+    "revision": "f6698fceefd5682efae6",
+    "url": "/dcloud/static/js/2.62c86b91.chunk.js"
+  },
+  {
+    "revision": "7716a58c2962e8c27abbb03c62b6d659",
+    "url": "/dcloud/static/js/2.62c86b91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89e639ac30df8fec60e0",
+    "url": "/dcloud/static/js/3.cf8dc54b.chunk.js"
+  },
+  {
+    "revision": "ae4eb96ca59dba2679c6",
+    "url": "/dcloud/static/js/main.e03babae.chunk.js"
+  },
+  {
+    "revision": "d5c87e367b6c05edd640",
+    "url": "/dcloud/static/js/runtime-main.39f0e90c.js"
+  }
+]);
